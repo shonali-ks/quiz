04c363bf1b852c25d87109f9b72e36f3fe8172c5
shonali-ks <50069308+shonali-ks@users.noreply.github.com>
2019-08-31 17:10:17 +0530

    Update con.php

--- con.php
+++ con.php
@@ -1,5 +1,6 @@
 <?php
-$con=mysqli_connect("localhost","root","Shons@123","users");
+//root is the username and users is the name of the database
+$con=mysqli_connect("localhost","root",your_password,"users");
 if($con==false)
 {
 die("error in connection".mysqli_connect_error());
